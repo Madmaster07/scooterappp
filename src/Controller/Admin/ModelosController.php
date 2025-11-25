0f7962523c5db9ba09e015581e3e1c6c95041df4
Saul Beltran Gomez <saufjdj@gmail.com>
2025-11-24 19:54:03 -0600

    Proteger CRUDS

--- src/Controller/Admin/ModelosController.php
+++ src/Controller/Admin/ModelosController.php
@@ -1,102 +1,20 @@
-<?php
-declare(strict_types=1);
 
+<?php
 namespace App\Controller\Admin;
 
-use App\Controller\AppController;
 
-/**
- * Modelos Controller
- *
- * @property \App\Model\Table\ModelosTable $Modelos
- */
+use App\Controller\Admin\AppController; 
+
 class ModelosController extends AppController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
-    public function index()
-    {
-        $query = $this->Modelos->find();
-        $modelos = $this->paginate($query);
-
-        $this->set(compact('modelos'));
+    
+    public function index() {
+        
     }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Modelo id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $modelo = $this->Modelos->get($id, contain: ['Vehiculos']);
-        $this->set(compact('modelo'));
+    
+    
+    public function add() {
+        
     }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $modelo = $this->Modelos->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $modelo = $this->Modelos->patchEntity($modelo, $this->request->getData());
-            if ($this->Modelos->save($modelo)) {
-                $this->Flash->success(__('The modelo has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The modelo could not be saved. Please, try again.'));
-        }
-        $this->set(compact('modelo'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Modelo id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $modelo = $this->Modelos->get($id, contain: []);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $modelo = $this->Modelos->patchEntity($modelo, $this->request->getData());
-            if ($this->Modelos->save($modelo)) {
-                $this->Flash->success(__('The modelo has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The modelo could not be saved. Please, try again.'));
-        }
-        $this->set(compact('modelo'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Modelo id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $modelo = $this->Modelos->get($id);
-        if ($this->Modelos->delete($modelo)) {
-            $this->Flash->success(__('The modelo has been deleted.'));
-        } else {
-            $this->Flash->error(__('The modelo could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
+    
 }
